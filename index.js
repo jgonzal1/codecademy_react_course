4c1d4dfa2dca962291d04af33bf3e9a3ea124822
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2021-05-22 23:30:28 +0200

    + ended sololearn react course, markdownlint, # html names

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './my-app/src/reportWebVitals';
+
+/** @typedef {{}} */
+var ReactEvent;
+/** @typedef {{}} */
+var ReactObjectDefaultChildren;
 
 //#region
 /**
@@ -36,6 +41,11 @@ function displayGrowingNumber() {
   setInterval(show, 1000);
 }
 
+/**
+ * @param {ReactObjectDefaultChildren} props - to pass down information
+ * from parent component to child component
+ * @returns 
+ */
 function Hello(props){return <p>Hello, {props.name}!</p>;}
 class Hello1 extends React.Component{render(){return <p>Hello, {this.props.name}!</p>;}}
 class Hello2 extends React.Component{
@@ -61,6 +71,9 @@ class Counter1 extends React.Component {
     </div>;
   }
 }
+/**
+ * use `state` always when managing forms
+ */
 function setStateUsage() {
   const el = <Counter/>;
   ReactDOM.render(el, document.getElementById('root'));
@@ -68,6 +81,10 @@ function setStateUsage() {
 
 class Person extends React.Component{state={name:this.props.name}}
 
+/**
+ * useState podría inicializarse en `""`.
+ * @returns 
+ */
 function Hello3() {
   const [name, setName] = useState("David");
   const [count, setCount] = useState(42);
@@ -102,6 +119,9 @@ class Counter2 extends React.Component {
   // Similarly, the componentWillUnmount() lifecycle method
   // is called right before the component is removed from the DOM.
   // It can be used to free up resources taken by the component.
+  componentDidUpdate() {
+    alert("Number of clicks: " + this.state.counter);
+  }
   render() {
     return <div>
     <p>{this.state.counter}</p>
@@ -109,8 +129,132 @@ class Counter2 extends React.Component {
     </div>;
   }
 }
+
+/**
+ * @returns alert every time counter changes (either mount, update, unmount)
+ */
+function Counter3() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("Number of clicks: " + counter);
+  });
+
+  function increment() {
+    setCounter(counter+1);
+  }
+  return <div>
+  <p>{counter}</p>
+  <button onClick={increment}>Increment</button>
+  </div>;
+}
+
 function componentDidMountUsage() {
   ReactDOM.render(<Counter/>, document.getElementById('root'));
 }
+
+/**
+ * @param {number} n 
+ * @returns {number} `n` fixed to 2 decimal numbers.
+ */
+function redondear2Dec(n) {
+  return parseFloat(n.toFixed(2));
+}
+
+function Converter() {
+  const [km, setKm] = useState(0);
+
+  /**
+   * @param {ReactEvent} e 
+   */
+  function handleChange(e) {
+    setKm(e.target.value);
+  }
+  function convert(km) {
+    return (km/1.609).toFixed(2);
+  }
+
+  return <div>
+  <input type="text" value={km}
+     onChange={handleChange} />
+  <p> {km} km is {convert(km)} miles </p>
+  </div>;
+}
+
+/**
+ * e.preventDefault() statement prevents the default behavior of the form.
+ * Forms, by default, reloads the page when submitted.
+ * In JavaScript we would use return false for that
+ * In React we need to call preventDefault().
+ * @param {ReactEvent} e
+ */
+function handleSubmitWithPreventDefault (e) {
+  setSum(sum + Number(num));
+  e.preventDefault();
+}
+
+/**
+ * Its meant to share state for children,
+ * 
+ * so the state from a form already submitted
+ * 
+ * can be accesed for other child
+ * @param {ReactObjectDefaultChildren} props 
+ * @returns `<div/>`
+ */
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson}/>
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+
+function AddPersonForm(props) {
+  const [ person, setPerson ] = useState("");
+    
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+  
+  /**
+   * Uses parent function `addPerson`
+   * @param {ReactEvent} e 
+   */
+  function handleSubmit(e) {
+    props.handleSubmit(person);
+    setPerson('');
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+        placeholder="Add new contact" 
+        onChange={handleChange} 
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+/**
+  * If the parent component uses this to pass down its `calculate()` function:
+  * 
+  * ```js
+  * <Child doStuff={calculate}/>
+  * ```
+  * 
+  * The child call that function as `props.doStuff()`
+  */
+function howToPassFxToChildren() {}
+
+
 
 reportWebVitals(console.log);
